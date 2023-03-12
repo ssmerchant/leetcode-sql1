0cf1b0d5766c15720aac55bd48ac798b40c14fdc
Samuel Scott Merchant <103539397+ssmerchant@users.noreply.github.com>
2023-03-11 19:58:12 -0600

    Add files via upload

---
+++ Day4/Second_Highest_Salary.sql
@@ -0,0 +1,20 @@
+'''
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key column for this table.
+Each row of this table contains information about the salary of an employee.
+
+ 
+
+Write an SQL query to report the second highest salary from the Employee table. If there is no second highest salary, the query should report null.
+'''
+--MySQL Solution
+
+SELECT MAX(salary) AS SecondHighestSalary FROM Employee
+WHERE salary NOT IN (SELECT MAX(salary) FROM Employee);
