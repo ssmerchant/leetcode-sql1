0542b3f77e3787495efba9cd6d300d8b73d55c98
Samuel Scott Merchant <103539397+ssmerchant@users.noreply.github.com>
2023-03-20 21:28:16 -0500

    Add files via upload

---
+++ Day10/1084_Sales_Analysis_III.sql
@@ -0,0 +1,41 @@
+'''
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
+| unit_price   | int     |
++--------------+---------+
+product_id is the primary key of this table.
+Each row of this table indicates the name and the price of each product.
+Table: Sales
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| seller_id   | int     |
+| product_id  | int     |
+| buyer_id    | int     |
+| sale_date   | date    |
+| quantity    | int     |
+| price       | int     |
++-------------+---------+
+This table has no primary key, it can have repeated rows.
+product_id is a foreign key to the Product table.
+Each row of this table contains some information about one sale.
+ 
+
+Write an SQL query that reports the products that were only sold in the first quarter of 2019. That is, between 2019-01-01 and 2019-03-31 inclusive.
+
+Return the result table in any order.
+'''
+--MySQL Solution
+
+SELECT Sales.product_id, product_name FROM Sales
+LEFT JOIN Product
+ON Sales.product_id = Product.product_id
+GROUP BY Sales.product_id
+HAVING MIN(sale_date) >= CAST('2019-01-01' AS DATE)
+AND MAX(sale_date) <= CAST('2019-03-31' AS DATE)
