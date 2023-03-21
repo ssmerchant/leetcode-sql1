924dcece3d0dc080817a352ea40b703838cfa799
Samuel Scott Merchant <103539397+ssmerchant@users.noreply.github.com>
2023-03-20 21:25:28 -0500

    Add files via upload

---
+++ Day6/197_Rising_Temperature.sql
@@ -0,0 +1,23 @@
+'''
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+ 
+
+Write an SQL query to find all dates Id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+'''
+--MySQL Solution
+
+SELECT w1.id FROM Weather w1, Weather w2
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1
+AND w1.temperature > w2.temperature
