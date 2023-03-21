3d40d7ec463f1b57224ea2ad212385ae7830e5e8
Samuel Scott Merchant <103539397+ssmerchant@users.noreply.github.com>
2023-03-20 21:27:08 -0500

    Add files via upload

---
+++ 1158_Market_Analysis_I.sql
@@ -0,0 +1,52 @@
+"""
+Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| join_date      | date    |
+| favorite_brand | varchar |
++----------------+---------+
+user_id is the primary key of this table.
+This table has the info of the users of an online shopping website where users can sell and buy items.
+ 
+
+Table: Orders
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| order_id      | int     |
+| order_date    | date    |
+| item_id       | int     |
+| buyer_id      | int     |
+| seller_id     | int     |
++---------------+---------+
+order_id is the primary key of this table.
+item_id is a foreign key to the Items table.
+buyer_id and seller_id are foreign keys to the Users table.
+ 
+
+Table: Items
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| item_id       | int     |
+| item_brand    | varchar |
++---------------+---------+
+item_id is the primary key of this table.
+ 
+
+Write an SQL query to find for each user, the join date and the number of orders they made as a buyer in 2019.
+
+Return the result table in any order.
+"""
+--MySQL Solution
+
+SELECT Users.user_id AS buyer_id, Users.join_date, SUM(if(order_date IS NOT NULL, 1, 0)) AS orders_in_2019
+FROM Users
+LEFT JOIN
+Orders ON Users.user_id = Orders.buyer_id AND YEAR(order_date) = '2019'
+GROUP BY Users.user_id
